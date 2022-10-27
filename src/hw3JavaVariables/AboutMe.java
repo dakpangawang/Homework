ea32a7fc1beafeaa1b9cf2d4dfabd8256e5fcb0a
dakpangawang <ngwngdkp@gmail.com>
2022-10-26 21:05:50 -0400

    updated homework 03

--- src/hw3JavaVariables/AboutMe.java
+++ src/hw3JavaVariables/AboutMe.java
@@ -2,16 +2,22 @@ package hw3JavaVariables;
 
 public class AboutMe {
 
-	boolean isRight;// Variable declared
-//	Variable initialised
-	String name = "Ngawang";
-	byte age = 16;
-	short rollNumber = 5602;
-	int phoneNumber = 123456789;
-	long accNumber = 12345678991l;
-	char gender = 'M';
-	float balance = 1001.245f;
-	double piValue = 3.14285714;
-	boolean button = false;
+	public boolean isRight;   // Variable declared
+	// Variable initialised
+	public String name = "Ngawang";
+	public byte age = 16;
+	public short rollNumber = 5602;
+	public int phoneNumber = 123456789;
+	public long accNumber = 12345678991l;
+	public char gender = 'M';
+	public float balance = 1001.245f;
+	public double piValue = 3.14285714;
+	public boolean button = false;
+
+	public static void main(String[] args) {
+		AboutMe obj = new AboutMe();
+		System.out.println("my name is: " + obj.name + "\nMy Gender is: " + obj.gender);
+
+	}
 
 }
