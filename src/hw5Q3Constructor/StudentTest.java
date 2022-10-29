dd8a867c50f2b9a69b43050b6eeb069fd26f0c3b
dakpangawang <ngwngdkp@gmail.com>
2022-10-29 18:00:26 -0400

    hw5Q3Constructor

---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,10 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		Student student2 = new Student("Ngawang Dakpa",20220431,'M',true,4.00f);
+	}
+
+}
